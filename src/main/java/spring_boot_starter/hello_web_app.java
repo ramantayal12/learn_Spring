d4e150c2ef6ccea58a990c50b50f7fb9f6453b04
ramantayal12 <ramantayal12@gmail.com>
2021-02-25 13:08:14 +0530

    Spring Boot Application of basics is Completed

---
+++ src/main/java/spring_boot_starter/hello_web_app.java
@@ -0,0 +1,13 @@
+package spring_boot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class hello_web_app {
+
+    public static void main(String []args ){
+
+        SpringApplication.run(hello_web_app.class,args);
+    }
+}
